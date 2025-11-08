39925a73da85b8e17b54c7b16e9fb035ead570a9
Еркус Сагуев <e.saguev@192.168.10.16>
2025-11-08 17:35:36 +0500

    dark mode login page

--- js/login.js
+++ js/login.js
@@ -90,3 +90,22 @@ signInForm.addEventListener('submit', async (e) => {
   }
 });
 
+const themeToggleBtn = document.getElementById('theme-toggle');
+
+if (themeToggleBtn) {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    document.body.classList.add('dark-theme');
+    themeToggleBtn.textContent = '☀️'; 
+  } else {
+    themeToggleBtn.textContent = '🌙'; 
+  }
+
+
+  themeToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    const isDark = document.body.classList.contains('dark-theme');
+    themeToggleBtn.textContent = isDark ? '☀️' : '🌙';
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  });
+}  
